6702db5ed85260d6bcff85d2d2e477f9226bc812
Thanhson89 <43216036+Thanhson89@users.noreply.github.com>
2020-01-04 10:07:18 -0700

    edit

--- test_fxp.py
+++ test_fxp.py
@@ -1,25 +1,22 @@
 
 
-from pysmt.shortcuts import SBV, Symbol, is_valid, Equals, Not
+from pysmt.shortcuts import *
 
 from pysmt.typing import UFXPType, SFXPType, FXP_OM, FXP_RM
 
-from pysmt.shortcuts import UFXPAdd, UFXPSub, UFXPMul, UFXPDiv, \
-                            SFXPAdd, SFXPSub, SFXPMul, SFXPDiv, \
-                            UFXP, SFXP, BV, ST, WP, RU, RD, \
-                            get_model
 
-from pysmt.rewritings import get_fp_bv_converter
+from pysmt.rewritings import *
 
 
-x = Symbol("x", UFXPType(10,5))
+x = Symbol("x", UFXPType(2,1))
 
-y = Symbol("y", UFXPType(10,5))
-z = Symbol("z", UFXPType(10,5))
+#y = Symbol("y", UFXPType(10,5))
+#z = Symbol("z", UFXPType(10,5))
 
 o = Symbol('o', FXP_OM)
 
 r = Symbol('r', FXP_RM)
+
 
 #z = Equals(x, y)
 
@@ -27,23 +24,30 @@ r = Symbol('r', FXP_RM)
 
 #z = UFXPLt(x, y)
 
-zz = UFXPAdd(o, x, y)
-zzz = UFXPSub(WP, x, zz)
-zzzz = UFXPMul(o, r, x, y)
-zzzzz = UFXPDiv(o, r, x, y)
+#zz = UFXPAdd(o, x, y)
+#zzz = UFXPSub(WP, x, zz)
+#zzzz = UFXPMul(o, r, x, y)
+#zzzzz = UFXPDiv(o, r, x, y)
 
-kk = UFXPMul(o, r, x, y)
+#kk = UFXPMul(o, r, x, y)
 
-k = BV(1, 2)
+b1 = BV(1, 2)
+b2 = BV(3, 2)
+
 conv =  get_fp_bv_converter()
-model = get_model(conv.convert(Equals(zz, kk)))
+
+#a = UFXP(b1,2)
+b = UFXP(b2,1)
+print(conv.convert(b))
+#res = UFXPAdd(ST,a,b)
+model = get_model(conv.convert(UFXPLE(x, b)))
 if model:
-    print 'sat'
-    print model
+    print ('sat')
+    print (model)
 else:
-    print 'unsat'
-#print conv.convert(zz)
-print conv.convert(zzz)
+    print ('unsat')
+#print (conv.convert(res))
+#print conv.convert(zzz)
 #kk = Equals(zzz, zzz)
 #kkk = Equals(k,k)
 #print conv.convert(kk)
